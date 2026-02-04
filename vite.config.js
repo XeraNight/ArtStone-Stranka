9212e33155be38bb3ea4bc531dd91aecc52659fa
Jakub Kalina <149116463+XeraNight@users.noreply.github.com>
2026-02-04 20:27:41 +0100

    fix: github pages workaround - commit dist folder and redirect script

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/ArtStone-Stranka/",
+  base: "/ArtStone-Stranka/dist/",
   build: {
     rollupOptions: {
       input: {
