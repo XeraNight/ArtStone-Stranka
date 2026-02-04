5975ccff24aaa863d029b3ffff197a339e3625be
Jakub Kalina <149116463+XeraNight@users.noreply.github.com>
2026-02-04 20:36:37 +0100

    fix: restore github pages redirect hack

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "/ArtStone-Stranka/dist/",
   build: {
     rollupOptions: {
       input: {
